f38f5978f7dd76ba3d63900365fcb677e9575550
lorenzo-confalonieri <confalonieri.lorenzo@iisgalvanimi.edu.it>
2025-03-26 09:11:53 +0000

    versione 1

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 function startGame() {
-  console.log("ciao")
   myGameArea.start();
   myGameArea.draw(redSquare);
-}
-
-var myGameArea = {
-  canvas: document.createElement("canvas"),
-  start: function () {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-  }
-
 }
 var redSquare = {
   width: 20,
@@ -22,6 +10,49 @@ var redSquare = {
   color: "red"
 };
 
+var myGameArea = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  draw:function(component){
+    this.context.fillStyle=component.color;
+    this.context.fillRect(
+      component.x,
+      component.y,
+      component.width,
+      component.height
 
+    );
+  },
+  clear:function(){
+    this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+  }
+};
+function updateGameArea() {
+  myGameArea.clear();
+  myGameArea.draw(redSquare);
+}
+function moveup() {
+  redSquare.y -= 30;
+  updateGameArea();
+}
 
+function movedown() {
+  redSquare.y += 30;
+  updateGameArea();
+}
 
+function moveleft() {
+  redSquare.x -= 30;
+  updateGameArea();
+}
+
+function moveright() {
+  redSquare.x += 30;
+  updateGameArea();
+}
